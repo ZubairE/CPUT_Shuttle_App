07410a4d0b35ec6b25b98e78c8c79935d04fad01
breySean <breytonernstzen@gmail.com>
2021-05-02 10:34:12 +0200

    some code added in the login class changes made in the login xml

--- app/src/main/java/za/ac/cput_shuttleapp/Login.java
+++ app/src/main/java/za/ac/cput_shuttleapp/Login.java
@@ -3,12 +3,21 @@ package za.ac.cput_shuttleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    EditText studentNumber,studentPassword;
+    Button btnLogin,btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        studentNumber = findViewById(R.id.editTextPhone);
+        studentPassword = findViewById(R.id.editTextTextPassword);
+        btnLogin = findViewById(R.id.login);
+        btnRegister = findViewById(R.id.register);
     }
 }
