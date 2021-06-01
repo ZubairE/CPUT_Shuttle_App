424b02ef394d464bca0d15f71938efa90a3ac3d6
breySean <breytonernstzen@gmail.com>
2021-06-01 23:58:10 +0200

    Dummy activity added if login is sucess.

--- app/src/main/java/za/ac/cput_shuttleapp/Login.java
+++ app/src/main/java/za/ac/cput_shuttleapp/Login.java
@@ -2,6 +2,7 @@ package za.ac.cput_shuttleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -32,14 +33,23 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(TextUtils.isEmpty(studentNumber.getText().toString()) || TextUtils.isEmpty(studentPassword.getText().toString())){
+                if (TextUtils.isEmpty(studentNumber.getText().toString()) || TextUtils.isEmpty(studentPassword.getText().toString())) {
                     Toast.makeText(Login.this, "Fields Cannot Be Empty", Toast.LENGTH_LONG).show();
-                }else if(studentNumber.getText().toString().equals(validStudentNumber) || studentPassword.getText().toString().equals(validPassword)){
-                        Toast.makeText(Login.this, "Welcome to CPUT-Shuttle App!", Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(Login.this, "Student does not exist.Register Instead?",Toast.LENGTH_LONG).show();
+                } else if (studentNumber.getText().toString().equals(validStudentNumber) || studentPassword.getText().toString().equals(validPassword)) {
+                    Toast.makeText(Login.this, "Welcome to CPUT-Shuttle App!", Toast.LENGTH_LONG).show();
+                    mainActivity();//Opens next page(dummy page) when login was success
+                } else {
+                    Toast.makeText(Login.this, "Student does not exist.Register Instead?", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
-}
+
+    //This is just a dummy link activity. Takes user to next page when login was success
+    public void mainActivity() {
+        Intent open = new Intent(this, MainActivity.class);
+        startActivity(open);
+
+    }
+    }
+
