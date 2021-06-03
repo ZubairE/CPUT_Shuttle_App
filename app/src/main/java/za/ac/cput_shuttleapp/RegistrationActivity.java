e1d7d7d2fb868261502a401dd3bc2ad3b650c24a
ZubairE <esau.zubair98@gmail.com>
2021-06-03 15:53:02 +0200

    Declaring the variables

--- app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivity.java
+++ app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivity.java
@@ -3,12 +3,46 @@ package za.ac.cput_shuttleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+  private EditText eRegNumber;
+  private EditText eRegPassword;
+  private Button eRegister;
+  private Button eCancel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        eRegNumber = findViewById(R.id.etNumber);
+        eRegPassword = findViewById(R.id.etRegPassword);
+        eRegister = findViewById(R.id.btnRegstrate);
+        eCancel = findViewById(R.id.btnCancels);
+
+        eRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String regStudentnumber = eRegNumber.getText().toString();
+                String regPassword = eRegPassword.getText().toString();
+
+            }
+        });
+
+        eCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
