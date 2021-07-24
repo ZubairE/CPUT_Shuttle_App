e8ae1c47d0fe34566409766ca4e8a64f5000c79f
breySean <breytonernstzen@gmail.com>
2021-07-24 22:20:56 +0200

    register page created

---
+++ app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivty.java
@@ -0,0 +1,14 @@
+package za.ac.cput_shuttleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_activty);
+    }
+}
