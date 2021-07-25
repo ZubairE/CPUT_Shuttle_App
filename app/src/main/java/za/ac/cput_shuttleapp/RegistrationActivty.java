cd8cf83dc872482ce711522a0688f033e98831d0
breySean <breytonernstzen@gmail.com>
2021-07-25 13:29:27 +0200

    methods added on register page

--- app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivty.java
+++ app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivty.java
@@ -2,13 +2,31 @@ package za.ac.cput_shuttleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivty extends AppCompatActivity {
+
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_activty);
+
+        btnBack = findViewById(R.id.back);
+
+        btnBack.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                goBack();
+            }
+        });
     }
-}
+    public void goBack(){
+        Intent back = new Intent(this, Login.class);
+        startActivity(back);
+        }
+    }
