f0b19bc3078c5f62d4eb6ed8117b68dccf81b5d1
breySean <breytonernstzen@gmail.com>
2021-07-26 20:19:56 +0200

    Code fixed

--- app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivity.java
+++ app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivity.java
@@ -2,55 +2,65 @@ package za.ac.cput_shuttleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
-
-  private EditText eRegNumber;
-  private EditText eRegPassword;
-  private Button eRegister;
-  private Button eCancel;
-
+    EditText txtFirstName;
+    EditText txtLastName;
+    EditText studNumber;
+    EditText password;
+    EditText confirmPW;
+    Button btnRegister;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_registration_activty);
 
+        txtFirstName = findViewById(R.id.firstname);
+        txtLastName = findViewById(R.id.lastname);
+        studNumber = findViewById(R.id.stuNum);
+        password = findViewById(R.id.pw);
+        confirmPW = findViewById(R.id.cpw);
+        btnRegister = findViewById(R.id.register);
+        btnBack = findViewById(R.id.back);
 
-
-        setContentView(R.layout.activity_registration);
-
-        eRegNumber = findViewById(R.id.etNumber);
-        eRegPassword = findViewById(R.id.etRegPassword);
-        eRegister = findViewById(R.id.btnRegstrate);
-        eCancel = findViewById(R.id.btnCancels);
-
-        eRegister.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String regStudentnumber = eRegNumber.getText().toString();
-                String regPassword = eRegPassword.getText().toString();
+                if (TextUtils.isEmpty(txtFirstName.getText().toString()) || TextUtils.isEmpty(txtLastName.getText().toString()) ||
+                        TextUtils.isEmpty(studNumber.getText().toString()) || TextUtils.isEmpty(password.getText().toString()) ||
+                        TextUtils.isEmpty(confirmPW.getText().toString())) {
+                    Toast.makeText(RegistrationActivity.this, "Please complete all details", Toast.LENGTH_LONG).show();
+                }//Still need code as soon as database is completed
 
             }
+
         });
 
-        eCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
+        btnBack = findViewById(R.id.back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
-
+                goBack();
             }
         });
+    }
 
-
-
+    public void goBack() {
+        Intent back = new Intent(this, Login.class);
+        startActivity(back);
     }
 }
+
+
