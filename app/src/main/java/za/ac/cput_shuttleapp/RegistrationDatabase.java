438c2ca653653db65d81162f88a48521519738c4
breySean <breytonernstzen@gmail.com>
2021-07-28 19:06:41 +0200

    database class added

--- app/src/main/java/za/ac/cput_shuttleapp/RegistrationDatabase.java
+++ app/src/main/java/za/ac/cput_shuttleapp/RegistrationDatabase.java
@@ -1,4 +1,23 @@
 package za.ac.cput_shuttleapp;
 
-public class RegistrationDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class RegistrationDatabase extends SQLiteOpenHelper {
+    public RegistrationDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
