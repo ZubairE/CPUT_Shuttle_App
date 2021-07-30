09cd11a06d69a65cfdda772f51a7ac77a9ed653c
breySean <breytonernstzen@gmail.com>
2021-07-30 21:43:28 +0200

    itent filter added

--- app/src/main/java/za/ac/cput_shuttleapp/Timetable.java
+++ app/src/main/java/za/ac/cput_shuttleapp/Timetable.java
@@ -2,7 +2,11 @@ package za.ac.cput_shuttleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class Timetable extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class Timetable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Locations, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.Locations2, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner2.setAdapter(adapter1);
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.Days, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner3.setAdapter(adapter2);
+
+        Button btn =(Button) findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+            Intent intent2 = new Intent(this,ShuttleTimes.class);
+            startActivity(intent2);
+
+        });
     }
 }
