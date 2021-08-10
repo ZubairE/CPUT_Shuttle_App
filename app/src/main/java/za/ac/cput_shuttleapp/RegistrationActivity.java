d8c4c9f839e06f7bce3ad5f05a555b7c3a3ed24f
breySean <breytonernstzen@gmail.com>
2021-08-10 13:56:02 +0200

    password visibility set on registration page

--- app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivity.java
+++ app/src/main/java/za/ac/cput_shuttleapp/RegistrationActivity.java
@@ -8,13 +8,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class RegistrationActivity extends AppCompatActivity {
     SQLiteOpenHelper db_OpenHelper; //Calls the database class
@@ -23,11 +28,21 @@ public class RegistrationActivity extends AppCompatActivity {
     EditText txtLastName;
     EditText studNumber;
     EditText password;
+    CheckBox chk;
+    CheckBox chk2;
     EditText confirmPW;
     Button btnRegister;
     Button btnBack;
 
-    long sn;
+    //Take out if not working
+    public static final Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+            "(?=.*[0-9])" +             //The pw requires at least one digit
+            "(?=.*[a-z])" +             //The pw requires at least one small letter
+            "(?=.*[A-Z])" +             //The pw requires at least one capital letter
+            "(?=.*[@#$%^&+=!_])" +        //The pw requires at least one special character
+            "(?=\\S+$)" +               //No white spaces allowed
+            ".{8,15}" +                   //The password requires at least 8 characters
+            "$");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +54,37 @@ public class RegistrationActivity extends AppCompatActivity {
         txtLastName = findViewById(R.id.lastname);
         studNumber = findViewById(R.id.stuNum);
         password = findViewById(R.id.pw);
+        chk = findViewById(R.id.chk);
         confirmPW = findViewById(R.id.cpw);
+        chk2 = findViewById(R.id.chk2);
         btnRegister = findViewById(R.id.register);
         btnBack = findViewById(R.id.back);
+
+        chk2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+
+                if(b){
+                    confirmPW.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+
+                }else{
+                    confirmPW.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
+
+        chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                        password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+
+                }else{
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,12 +100,22 @@ public class RegistrationActivity extends AppCompatActivity {
                         TextUtils.isEmpty(studNumber.getText().toString()) || TextUtils.isEmpty(password.getText().toString()) ||
                         TextUtils.isEmpty(confirmPW.getText().toString())) {
                     Toast.makeText(RegistrationActivity.this, "Please complete all details", Toast.LENGTH_LONG).show();
-                    //Remove else-if statement if it is not working or cause problems.
+
+                }else if(!PASSWORD_PATTERN.matcher(userPw).matches()) {
+                    password.setError("Password must be at least 8 characters long.Requires at least one digit, one lowercase letter, one uppercase, and one special character");
+
+                }else if(!PASSWORD_PATTERN.matcher(confirmUserPw).matches()) {
+                    confirmPW.setError("Password must be at least 8 characters long.Requires at least one digit, one lowercase letter, one uppercase, and one special character");
+
+                /*}else if(!password.equals(confirmPW)){
+                    confirmPW.setError("Oops! Passwords does not match");*/
+
                 }else if(txtFirstName.getText().toString().equals(name)||txtLastName.getText().toString().equals(surname)||
-                        studNumber.getText().toString().equals(stuNum)||password.getText().toString().equals(confirmPW)&&
-                        confirmPW.getText().toString().equals(password)){
-                    Toast.makeText(RegistrationActivity.this,"Registration Success",Toast.LENGTH_LONG).show();
+                        studNumber.getText().toString().equals(stuNum)||password.getText().toString().equals(userPw)&&
+                        confirmPW.getText().toString().equals(confirmUserPw)) {
+                    Toast.makeText(RegistrationActivity.this, "Registration Success", Toast.LENGTH_LONG).show();
                     login();
+
                 }else{
                     Toast.makeText(RegistrationActivity.this,"Password does not match",Toast.LENGTH_LONG).show();
                 }
@@ -73,6 +126,7 @@ public class RegistrationActivity extends AppCompatActivity {
         btnBack.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
+
                 goBack();
             }
         });
@@ -106,7 +160,6 @@ public class RegistrationActivity extends AppCompatActivity {
         //Take this statement out if not working.Added on 6th August 2021
 
     }
-
     //This method may cause problems. Take it out if it mess up the program. Added on 6th of August 2021.
 
         }
