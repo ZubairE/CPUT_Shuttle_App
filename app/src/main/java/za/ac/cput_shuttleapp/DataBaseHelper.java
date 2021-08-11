ebd63eb278fa2c5bb55c0316565f36392602d799
breySean <breytonernstzen@gmail.com>
2021-08-11 20:11:48 +0200

    database class configured

--- app/src/main/java/za/ac/cput_shuttleapp/DataBaseHelper.java
+++ app/src/main/java/za/ac/cput_shuttleapp/DataBaseHelper.java
@@ -8,13 +8,13 @@ import androidx.annotation.Nullable;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "student_Registration.db";
-    public static final String DB_TABLE_NAME = "studenDetails";
+    public static final String MY_DB_TABLE_NAME = "studenDetails";
     public static final String COLUMN_1 = "ID";
     public static final String COLUMN_2 = "First_Name";
     public static final String COLUMN_3 = "Last_Name";
     public static final String COLUMN_4 = "Student_Number";
     public static final String COLUMN_5 = "Password";
-    public static final String COLUMN_6 = "Cellphone Number";
+    public static final String COLUMN_6 = "Cell_No";
 
     public DataBaseHelper(Context context) {
         super(context, DB_NAME, null,  1);
@@ -22,12 +22,12 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + DB_TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, First_Name TEXT, Last_Name TEXT, Student_Number TEXT, Password TEXT, Cell_No TEXT)");
+        db.execSQL("CREATE TABLE " + MY_DB_TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, First_Name TEXT, Last_Name TEXT, Student_Number TEXT, Password TEXT, Cell_No TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + MY_DB_TABLE_NAME);
         onCreate(db);
     }
 }
