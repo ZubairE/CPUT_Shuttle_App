8b20d1490434ea844d5959583c65b3243a512ea9
DemiFarquhar <80620054+DemiFarquhar@users.noreply.github.com>
2021-08-24 17:31:44 +0200

    Database

--- app/src/main/java/za/ac/cput_shuttleapp/Database.java
+++ app/src/main/java/za/ac/cput_shuttleapp/Database.java
@@ -5,31 +5,40 @@ import android.app.DownloadManager;
 import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.text.format.Time;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
+import androidx.annotation.FloatRange;
 import androidx.annotation.Nullable;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class Database extends SQLiteOpenHelper{
 
- private static final String DATABASE_NAME="myDatabase";
- private static final String TABLE_NAME="disabledBooking";
- private static final int DATABASE_Version=1;
- private static final String DISABILITY="disability";
- private static final String FROM="From";
- private static final String To="To";
- private static final String DATE_PICKER_DIALOG="date";
- private static final String TIME_PICKER="time";
- private static final String CREATE_TABLE = "CREATE"+TABLE_NAME+
-         "("+DISABILITY+"VARCHAR(255),"+FROM+"VARCHAR(255),"+To+"VARCHAR(255),"+DATE_PICKER_DIALOG+"TEXT,"+TIME_PICKER+"TEXT);";
- private static  final String DROP_TABLE="DROP TABLE IF EXISTS "+TABLE_NAME;
- private Context context;
+public class Database extends SQLiteOpenHelper {
 
- public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    private static final String DATABASE_NAME = "myDatabase";
+    private static final String TABLE_NAME = "disabledBooking";
+    private static final int DATABASE_Version = 1;
+    private static final String BOOKING_ID = "booking_Id";
+    private static final String DISABILITY = "disability";
+    private static final String FROM = "From";
+    private static final String To = "To";
+    private static final String DATE = "date";
+    private static final String TIME = "time";
+    private static final String CREATE_TABLE = "CREATE" + TABLE_NAME +
+            "(" + DISABILITY + "VARCHAR(255)," + FROM + "VARCHAR(255)," + To + "VARCHAR(255)," + DATE + "TEXT," + TIME + "TEXT);";
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    private Context context;
+
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
 
     }
@@ -37,22 +46,89 @@ public class Database extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-      db.execSQL(CREATE_TABLE) ;
+        String query = "CREATE TABLE " + TABLE_NAME + "("
+                + BOOKING_ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + DISABILITY + "STRING,"
+                + FROM + "STRING,"
+                + To + "STRING, "
+                + DATE + "TEXT,"
+                + TIME + "TEXT)";
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-     db.execSQL(DROP_TABLE);
+        db.execSQL(DROP_TABLE);
 
+    }
+
+
+    public void insertData(Integer booking_Id, String DISABILITY, String FROM, String To, String DATE, String TIME) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(BOOKING_ID, BOOKING_ID);
+        values.put(DISABILITY, DISABILITY);
+        values.put(FROM, FROM);
+        values.put(To, To);
+        values.put(DATE, DATE);
+        values.put(TIME, TIME);
+        db.insert("disabledBooking ", null, values);
+    }
+
+    public String getData() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] colunms = {BOOKING_ID, DISABILITY, FROM, To, DATE, TIME};
+        Cursor cursor = db.query(TABLE_NAME, colunms, null, null, null, null, null);
+        StringBuffer buffer = new StringBuffer();
+        while (cursor.moveToNext()) {
+            int booking_Id = cursor.getInt(cursor.getColumnIndex(BOOKING_ID));
+            String disability = cursor.getString(cursor.getColumnIndex(DISABILITY));
+            String From = cursor.getString(cursor.getColumnIndex(FROM));
+            String TO = cursor.getString(cursor.getColumnIndex(To));
+            String date = cursor.getString(cursor.getColumnIndex(DATE));
+            String time = cursor.getString(cursor.getColumnIndex(TIME));
+        }
+        return buffer.toString();
+    }
+
+    public Integer delete(Integer booking_Id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("DisabledBooking", "booking_Id =?", new String[]{Integer.toString(booking_Id)});
+    }
+
+    public boolean updateBooking(Integer booking_Id, String DISABILITY, String FROM, String To, String DATE, String TIME) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(BOOKING_ID, BOOKING_ID);
+        values.put(DISABILITY, DISABILITY);
+        values.put(FROM, FROM);
+        values.put(To, To);
+        values.put(DATE, DATE);
+        values.put(TIME, TIME);
+        db.update("disabledBooking ", values, "Booking_Id = ?", new String[]{Integer.toString(booking_Id)});
+        return true;
+
+    }
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        int numRows= (int ) DatabaseUtils.queryNumEntries(db, TABLE_NAME);
+        return numRows;
+    }
+    public ArrayList<String> getAllDisabledBooking(){
+        ArrayList<String> array_List =new ArrayList<String>();
+
+        SQLiteDatabase db=this.getReadableDatabase();
+        Cursor res =db.rawQuery("select *from disabledBooking",null);
+        res.moveToFirst();
+
+        while (res.isAfterLast()== false){
+            array_List.add(res.getString (res.getColumnIndex(TABLE_NAME)));
+            res.moveToNext();
+        }
+        return array_List;
     }
 }
 
-public void insertData(String disability, String From, )
 
 
-
-
-
-
-
-}
