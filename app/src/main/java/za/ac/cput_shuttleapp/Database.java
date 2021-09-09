9835c95546bb1773bba9e01953f1851ddec52354
DemiFarquhar <80620054+DemiFarquhar@users.noreply.github.com>
2021-09-09 21:08:00 +0200

    CHANGES MADE

--- app/src/main/java/za/ac/cput_shuttleapp/Database.java
+++ app/src/main/java/za/ac/cput_shuttleapp/Database.java
@@ -24,107 +24,31 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
 
-
 public class Database extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "myDatabase";
-    private static final String TABLE_NAME = "disabledBooking";
-    private static final int DATABASE_Version = 1;
-    private static final String BOOKING_ID = "booking_Id";
-    private static final String DISABILITY = "disability";
-    private static final String FROM = "From";
-    private static final String To = "To";
-    private static final String DATE = "date";
-    private static final String TIME = "time";
-    private static final String CREATE_TABLE = "CREATE" + TABLE_NAME +
-            "(" + DISABILITY + "VARCHAR(255)," + FROM + "VARCHAR(255)," + To + "VARCHAR(255)," + DATE + "TEXT," + TIME + "TEXT);";
-    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
-    private Context context;
+    public static final String NAME = "DisabeledStudent.db";
+    public static final String TABLE = "Disabled";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "DISABILITY";
+    public static final String COL3 = "START";
+    public static final String COL4 = "DESTINATION";
+    public static final String COL5 = "DATE";
+    public static final String COL6 = "TIME";
 
-    public Database(@Nullable Context context) {
-        super(context,DATABASE_NAME,null,DATABASE_Version);
-    }
-
+    public Database(Context context){super (context,NAME,null,1);}
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + TABLE_NAME + "("
-                + BOOKING_ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + DISABILITY + "STRING,"
-                + FROM + "STRING,"
-                + To + "STRING, "
-                + DATE + "TEXT,"
-                + TIME + "TEXT)";
-        db.execSQL(query);
+        db.execSQL("CREATE TABLE " + TABLE + " (ID INTEGER,DISABILITY TEXT,START TEXT,DESTINATION TEXT,DATE TEXT,TIME TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(DROP_TABLE);
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
+        onCreate(db);
     }
+}
 
-
-    public void insertData(String DISABILITY, String FROM, String To, String DATE, String TIME) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(DISABILITY, DISABILITY);
-        values.put(FROM, FROM);
-        values.put(To, To);
-        values.put(DATE, DATE);
-        values.put(TIME, TIME);
-        db.insert("disabledBooking ", null, values);
-
-
-    }
-
-    public String getData() {
-        SQLiteDatabase db = this.getReadableDatabase();
-        String[] colunms = {BOOKING_ID, DISABILITY, FROM, To, DATE, TIME};
-        Cursor cursor = db.query(TABLE_NAME, colunms, null, null, null, null, null);
-        StringBuffer buffer = new StringBuffer();
-        while (cursor.moveToNext()) {
-            int booking_Id = cursor.getInt(cursor.getColumnIndex(BOOKING_ID));
-            String disability = cursor.getString(cursor.getColumnIndex(DISABILITY));
-            String From = cursor.getString(cursor.getColumnIndex(FROM));
-            String TO = cursor.getString(cursor.getColumnIndex(To));
-            String date = cursor.getString(cursor.getColumnIndex(DATE));
-            String time = cursor.getString(cursor.getColumnIndex(TIME));
-        }
-        return buffer.toString();
-    }
-
-    public Integer delete(Integer booking_Id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete("DisabledBooking", "booking_Id =?", new String[]{Integer.toString(booking_Id)});
-    }
-
-    public boolean updateBooking(Integer booking_Id, String DISABILITY, String FROM, String To, String DATE, String TIME) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(BOOKING_ID, BOOKING_ID);
-        values.put(DISABILITY, DISABILITY);
-        values.put(FROM, FROM);
-        values.put(To, To);
-        values.put(DATE, DATE);
-        values.put(TIME, TIME);
-        db.update("disabledBooking ", values, "Booking_Id = ?", new String[]{Integer.toString(booking_Id)});
-        return true;
-
-    }
-    public int numberOfRows(){
-        SQLiteDatabase db = this.getWritableDatabase();
-        int numRows= (int ) DatabaseUtils.queryNumEntries(db, TABLE_NAME);
-        return numRows;
-    }
-public  Cursor getAllData(){
-        SQLiteDatabase db=this.getWritableDatabase();
-        Cursor c=db.rawQuery("select* from " + TABLE_NAME, null);
-        return c;
-
-            }
-        }
 
 
 
--- app/src/main/java/za/ac/cput_shuttleapp/disabledStudentBooking.java
+++ app/src/main/java/za/ac/cput_shuttleapp/disabledStudentBooking.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,6 +23,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -28,156 +32,80 @@ import java.util.List;
 
 import static za.ac.cput_shuttleapp.R.drawable.logo;
 
-public class disabledStudentBooking extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class disabledStudentBooking extends AppCompatActivity{
+    SQLiteDatabase db;
+    SQLiteOpenHelper openHelper;
 
-    //Spinner
-    String[] disability = {"Intellectual disability", "Vision impairment", "Blindness", "Physical disability", "other"};
-    String[] from = {"District Six Campus", "Residence"};
-    String[] to = {"District Six Campus", "Residence"};
-
-    private Button buttonBack;
-    private Button buttonNext;
-    private Button buttonConfirm;
-    private Database database;
-    TextView date;
-    DatePickerDialog datePickerDialog;
-    TextView time;
-    TimePickerDialog mTimePicker;
-
+    Button buttonBack;
+    Button buttonNext;
+    Button buttonConfirm;
+    EditText txtDisability;
+    EditText txtFrom;
+    EditText txtTo;
+    EditText txtDate;
+    EditText txtTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disabled_student_booking);
+        openHelper = new Database(this);
 
-        database = new Database(this);
-        AddData();
+        txtDisability = findViewById(R.id.editTextTextMultiLine);
+        txtFrom = findViewById(R.id.editTextTextMultiLine2);
+        txtTo = findViewById(R.id.editTextTextMultiLine3);
+        txtDate = findViewById(R.id.editTextTextMultiLine4);
+        txtTime = findViewById(R.id.editTextTextMultiLine5);
+        buttonBack = findViewById(R.id.bck);
+        buttonConfirm = findViewById(R.id.cnfrm);
+        buttonNext = findViewById(R.id.nxt);
 
-        ImageView myImageView = (ImageView) findViewById(R.id.imageView);
-        myImageView.setImageResource(logo);
-
-        TextView tv = findViewById(R.id.textView);
-        tv.setMovementMethod(new ScrollingMovementMethod());
-        TextView tv2 = findViewById(R.id.textView2);
-        tv2.setMovementMethod(new ScrollingMovementMethod());
-
-        addListenerOnButton();
-
-
-        //Creating the instance of Spinner
-        Spinner disability = (Spinner) findViewById(R.id.spinner2);
-        disability.setOnItemSelectedListener(this);
-        TextView tv3 = findViewById(R.id.textView3);
-        tv3.setMovementMethod(new ScrollingMovementMethod());
-        //Creating the ArrayAdapter
-        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, (List) disability);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        disability.setAdapter(aa);
-
-        Spinner From = (Spinner) findViewById(R.id.spinner);
-        From.setOnItemSelectedListener(this);
-        TextView tv4 = findViewById(R.id.textView4);
-        tv4.setMovementMethod(new ScrollingMovementMethod());
-        ArrayAdapter aa2 = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, from);
-        aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        From.setAdapter(aa2);
-
-        Spinner To = (Spinner) findViewById(R.id.spinner);
-        To.setOnItemSelectedListener(this);
-        TextView tv5 = findViewById(R.id.textView5);
-        tv4.setMovementMethod(new ScrollingMovementMethod());
-        ArrayAdapter aa3 = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, to);
-        aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        To.setAdapter(aa3);
-
-        date = (TextView) findViewById(R.id.date);
-        date.setOnClickListener(new OnClickListener() {
-
-
+        buttonConfirm.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR);
-                int mMonth = c.get(Calendar.MONTH);
-                int mDay = c.get(Calendar.DAY_OF_MONTH);
-                datePickerDialog = new DatePickerDialog(disabledStudentBooking.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-                    }
-                }, mYear, mMonth, mDay);
-                datePickerDialog.show();
+                String dis = txtDisability.getText().toString();
+                String frm = txtFrom.getText().toString();
+                String to = txtTo.getText().toString();
+                String date = txtDate.getText().toString();
+                String time = txtTime.getText().toString();
+                db = openHelper.getWritableDatabase();
+
+                if (TextUtils.isEmpty(txtDisability.getText().toString()) || TextUtils.isEmpty(txtFrom.getText().toString()) ||
+                        TextUtils.isEmpty(txtTo.getText().toString()) || TextUtils.isEmpty(txtDate.getText().toString()) ||
+                        TextUtils.isEmpty(txtTime.getText().toString())) {
+                    Toast.makeText(disabledStudentBooking.this, "Complete all fields", Toast.LENGTH_LONG).show();
+
+                } else if (txtDisability.getText().toString().equals(dis) && (txtFrom.getText().toString().equals(frm) &&
+                        (txtTo.getText().toString().equals(to) && (txtDate.getText().toString().equals(date) && (txtTime.getText().toString().equals(time)))))) {
+                    addBooking(dis, frm, to, date, time);
+                    Toast.makeText(disabledStudentBooking.this, "Booking has been made", Toast.LENGTH_LONG).show();
+
+
+                }
             }
         });
 
-
-        time = (TextView) findViewById(R.id.time);
-        time.setOnClickListener(new OnClickListener() {
+        buttonNext.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar mCurrentTime = Calendar.getInstance();
-                int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mCurrentTime.get(Calendar.MINUTE);
-
-                mTimePicker = new TimePickerDialog(disabledStudentBooking.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        time.setText(selectedHour + ":" + selectedMinute);
-                    }
-                }, hour, minute, true);
-                mTimePicker.setTitle("Time");
-                mTimePicker.show();
+                System.exit(0);
             }
         });
     }
 
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    public void addBooking(String disability, String from, String to, String date, String time) {
+        ContentValues cv = new ContentValues();
+        cv.put(Database.COL2, disability);
+        cv.put(Database.COL3, from);
+        cv.put(Database.COL4, to);
+        cv.put(Database.COL5, date);
+        cv.put(Database.COL6, time);
+        long id = db.insert(Database.TABLE, null, cv);
 
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    public void addListenerOnButton() {
-        buttonBack = (Button) findViewById(R.id.button);
-        buttonNext = (Button) findViewById(R.id.buttonNext);
-        buttonConfirm = (Button) findViewById(R.id.buttonConfirm);
-        buttonBack.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-            }
-
-
-        });
-    }
-
-    private void AddData() {
-        buttonConfirm.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View v) {
-                String DISABILITY = disability.getClass().toString();
-                String FROM = from.getClass().toString();
-                String TO = to.getClass().toString();
-                String DATE = date.getClass().toString();
-                String TIME = time.getText().toString();
-                database.insertData(DISABILITY, FROM, TO, DATE, TIME);
-
-
-            }
-
-        });
-    }
 }
-
-
 
 
 
