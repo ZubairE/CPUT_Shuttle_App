d01a577d1c8a0d73568963d98733b85c6dddbe12
breySean <breytonernstzen@gmail.com>
2021-10-17 20:49:50 +0200

    Comments added on the 'BookingDatabase' page

--- app/src/main/java/za/ac/cput_shuttleapp/BookingDatabase.java
+++ app/src/main/java/za/ac/cput_shuttleapp/BookingDatabase.java
@@ -3,9 +3,10 @@ package za.ac.cput_shuttleapp;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
+//Database for booking section
 public class BookingDatabase extends SQLiteOpenHelper {
 
+    //Database columns for booking page
     public static final String DATABASE_NAME = "Booking.db";
     public static final String TABLE_NAME = "Booking_table";
     public static final String COL_1 = "ID";
@@ -20,11 +21,13 @@ public class BookingDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //SQL statement for creating the booking database table
         db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER,DEPARTURE TEXT, DESTINATION TEXT,TIME TEXT, DATE TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //SQL statement for deleting the table
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
